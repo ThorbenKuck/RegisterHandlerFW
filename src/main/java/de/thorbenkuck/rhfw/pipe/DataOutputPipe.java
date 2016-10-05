3ed9963e9d9181359e920ab1f380f4e3e9c4a9a8
thorben <thorben.kuck@gmail.com>
2016-10-05 13:00:26 +0200

    FastClasspathScanner implemented

--- src/main/java/de/thorbenkuck/rhfw/pipe/DataOutputPipe.java
+++ src/main/java/de/thorbenkuck/rhfw/pipe/DataOutputPipe.java
@@ -1,11 +1,15 @@
 package de.thorbenkuck.rhfw.pipe;
 
-import de.thorbenkuck.rhfw.handler.AnnotationScanner;
+import de.thorbenkuck.rhfw.annotations.RegisterModule;
+import io.github.lukehutch.fastclasspathscanner.FastClasspathScanner;
+import io.github.lukehutch.fastclasspathscanner.scanner.ScanResult;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class DataOutputPipe {
 
@@ -46,26 +50,18 @@ public class DataOutputPipe {
      * @return
      */
     public <T> T getModule(String name, Class<T> type) {
-        T toreturn = null;
+        T toReturn = null;
         Class<?> classToInstance = moduleContainerList.getObjectedModule(name).getClass();
         String classNameToInstance = classToInstance.getName();
         try {
             Class<?> clazz = Class.forName(classNameToInstance);
             Constructor<?> ctor = clazz.getConstructor();
             Object object = ctor.newInstance();
-            toreturn = type.cast(object);
-        } catch (InstantiationException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
+            toReturn = type.cast(object);
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | ClassNotFoundException | NoSuchMethodException e) {
             e.printStackTrace();
         }
-        return toreturn;
+        return toReturn;
     }
 
     public <T> T getModule(String name) {
@@ -87,7 +83,13 @@ public class DataOutputPipe {
     }
 
     public static void loadAnnotatedModules() {
-        List<Class<?>> allModules = AnnotationScanner.getInstance().getAllAnnotadedClasses();
+        FastClasspathScanner fastClasspathScanner = new FastClasspathScanner();
+        fastClasspathScanner.matchClassesWithAnnotation(RegisterModule.class, aClass -> {});
+
+        ScanResult scanResult = fastClasspathScanner.scan();
+        List<String> names = scanResult.getNamesOfClassesWithAnnotation(RegisterModule.class);
+        List<Class<?>> allModules = names.stream().map((Function<String, Class<?>>) DataOutputPipe::instantiateClass).collect(Collectors.toList());
+
         int numberAllModules = allModules.size();
 
         /* Warum nicht foreach?
@@ -96,12 +98,26 @@ public class DataOutputPipe {
          * Wenn an jeder Stelle eine foreach schleife verwendet werden würde, würder der Listeninterne Interator ziemlich durcheinander kommen.
          * Deswegen nutzen wir zum Iterieren der Listen besser normale for-schleifen.
          */
-        for(int i = 0 ; i < numberAllModules ; i++) {
+        for (Class<?> allModule : allModules) {
             try {
-                DataOutputPipe.add(allModules.get(i).getName(), allModules.get(i).newInstance());
+                DataOutputPipe.add(allModule.getName(), allModule.newInstance());
             } catch (InstantiationException | IllegalAccessException e) {
                 e.printStackTrace();
             }
+        }
+    }
+
+    /**
+     * TODO Exception-Handling
+     * @param className
+     * @return
+     */
+    private static Class<?> instantiateClass(String className) {
+        try {
+            return Class.forName(className);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            return null;
         }
     }
 
