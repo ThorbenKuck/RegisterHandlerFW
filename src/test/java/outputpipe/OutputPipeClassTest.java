8a83381d9a11ad0acf40f0f634bf28bd703c49a5
thorben <thorben.kuck@gmail.com>
2016-10-05 17:18:18 +0200

    DataOutputPipeTest created

--- src/test/java/outputpipe/OutputPipeClassTest.java
+++ src/test/java/outputpipe/OutputPipeClassTest.java
@@ -1,4 +1,28 @@
 package outputpipe;
 
+import de.thorbenkuck.rhfw.pipe.DataOutputPipe;
+import org.junit.BeforeClass;
+
+import static junit.framework.TestCase.assertNotNull;
+
 public class OutputPipeClassTest {
+
+    private static DataOutputPipe dataOutputPipe;
+
+    @BeforeClass
+    public static void setup() {
+        DataOutputPipe.loadAnnotatedModules();
+        dataOutputPipe = DataOutputPipe.getInstance();
+    }
+
+    @org.junit.Test
+    public void existsTest() {
+        assertNotNull(dataOutputPipe);
+    }
+
+    @org.junit.Test
+    public void testGetClassTestFromPipe() {
+        Test test = dataOutputPipe.getModule(Test.class.getName());
+    }
+
 }
---
+++ src/test/java/outputpipe/Test.java
@@ -0,0 +1,12 @@
+package outputpipe;
+
+import de.thorbenkuck.rhfw.interfaces.RegisterModuleInterface;
+
+public class Test implements RegisterModuleInterface {
+
+    private static int id = 1;
+
+    public int getID () {
+        return 1;
+    }
+}
