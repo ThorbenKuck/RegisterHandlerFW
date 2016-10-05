0fcd59471ea9b6b13b909650496960cd7bf9c584
thorben <thorben.kuck@gmail.com>
2016-10-05 17:23:34 +0200

    RegisterID copy-constructor added

--- src/main/java/de/thorbenkuck/rhfw/handler/register/RegisterID.java
+++ src/main/java/de/thorbenkuck/rhfw/handler/register/RegisterID.java
@@ -14,6 +14,29 @@ public class RegisterID {
         randomID();
     }
 
+    /**
+     * TODO-Later Exception-Handling
+     * @param toCopy
+     */
+    public RegisterID(RegisterID toCopy) {
+        if(RegisterID.allRegisterIDs.contains(toCopy.toString())) {
+            this.currentRegisterID = toCopy.toString();
+        } else {
+            // TODO-Later Exception-Handling
+            System.out.println("NO! This is not allowed.. Also, here will be thrown an exception!");
+        }
+    }
+
+    public RegisterID(Register toCopy) {
+        RegisterID idToCopy = toCopy.getRegisterId();
+        if(RegisterID.allRegisterIDs.contains(idToCopy.toString())) {
+            this.currentRegisterID = idToCopy.toString();
+        } else {
+            // TODO-Later Exception-Handling
+            System.out.println("NO! This is not allowed.. Also, here will be thrown an exception!");
+        }
+    }
+
     @Override
     public String toString() {
         return currentRegisterID;
