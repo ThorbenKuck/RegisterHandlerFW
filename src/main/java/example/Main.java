bb9fa5acd1acd7674d935e1bab321de0049ee9e8
thorben <thorben.kuck@gmail.com>
2017-01-23 14:05:23 +0100

    main is now a bit better readable

--- src/main/java/example/Main.java
+++ src/main/java/example/Main.java
@@ -11,26 +11,34 @@ import org.apache.logging.log4j.core.config.ConfigurationFactory;
 
 public class Main {
     public static void main(String[] args) {
-
-		ConfigurationFactory.setConfigurationFactory(new CustomLog4J2ConfigurationFactory());
-
-        DataOutputPipe.access().loadAnnotatedModules();
+    	setup();
 
         Register register = RegisterHandler.pullAndGetNewRegister(new TestTemplate());
 
-        System.out.println(register);
+        prepareTest(register);
 
         Tester tester = new Tester();
         Tester2 tester2 = new Tester2();
-
-        C c = register.pullModule(C.class.getName());
-
-        c.higher();
-        c.howMuch();
 
         for(int i = 0 ; i < 4 ; i++) {
             tester.run();
             tester2.run();
         }
     }
+
+    private static void prepareTest(Register register) {
+		C c = register.pullModule(C.class.getName());
+
+		c.higher();
+		c.howMuch();
+	}
+
+	private static void setup() {
+		ConfigurationFactory.setConfigurationFactory(new CustomLog4J2ConfigurationFactory());
+
+		// This is alternative.
+		// It will not help the test, neither is it necessary for the test to work.
+		// The following line simply shows the multiton-behavior for the DataOutputPipe
+		DataOutputPipe.access().loadAnnotatedModules();
+	}
 }
